1d1de4baeb3b7f26b477d6497546d51dcc94055a
Bot-on-Tapwater <mundabrandon@outlook.com>
2024-01-06 10:21:37 +0300

    Fix Union type annotation

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,21 +1,28 @@
 #!/usr/bin/env python3
-"""
-Cache class
-"""
+"""Implement class Cache"""
+
 import redis
 import uuid
-from typing import Union, Optional, Callable
+from typing import Union
 
 
 class Cache:
-    """class Cache"""
+    """Class Cache"""
+
     def __init__(self):
-        """creates a connection with db and clears cache"""
+        """
+        Initializes an instance of the class.
+        """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """Generates key and stores data in db"""
-        key = str(uuid.uuid4())
-        self._redis.set(key, data)
-        return key
+        """
+        A description of the entire function
+        its parameters, and its return types.
+        """
+        random_key = str(uuid.uuid4())
+
+        self._redis.set(random_key, data)
+
+        return random_key
