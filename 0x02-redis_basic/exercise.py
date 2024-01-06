9cf394c00ead9bb56d366c59289fc7cd5303704b
Bot-on-Tapwater <mundabrandon@outlook.com>
2024-01-06 10:11:45 +0300

    Change store type annotation

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 
 import redis
 import uuid
-from typing import Dict
+from typing import Union
 
 
 class Cache:
@@ -16,7 +16,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: str | bytes | int | float) -> str:
+    def store(self, data: Union[str | bytes | int | float]) -> str:
         """
         A description of the entire function
         its parameters, and its return types.
